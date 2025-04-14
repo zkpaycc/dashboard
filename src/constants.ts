aa224a561d3758b859f49850043db73f80600dd0
zk <team@zkpay.cc>
2025-04-14 21:46:10 +0700

    chore: update @zkpay/sdk and payment url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const PAYMENT_URL = "https://test.zkpay.cc/payments/";
+export const PAYMENT_URL = "https://channels.zkpay.cc/";
 export const WALLETCONNECT_ID = "4b565ec484f0fe04253d901177a5046d";
 export const REDIRECT_PATH_KEY = "redirectAfterLogin";
